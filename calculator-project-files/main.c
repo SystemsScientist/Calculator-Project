ccb4bd28e0fecc8c2af5dba78324a4cfe15c6226
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-30 07:01:59 -0500

    Updating main.c

--- calculator-project-files/main.c
+++ calculator-project-files/main.c
@@ -4,13 +4,14 @@
 
 float add(float, float);
 float subtract(float, float);
+float divide(float, float);
 float multiply(float, float);
 
 int main(void) {
 
 	float num1, num2;
 	float sum_result, sub_result;
-	float mult_result;
+	float div_result, mult_result;
 
 	printf("Enter the first number: ");
 	scanf("%f", &num1);
@@ -19,10 +20,13 @@ int main(void) {
 	scanf("%f", &num2);
 
 	sum_result = add(num1, num2);
-	printf("Sum of %.2f and %.2f is %.2f\n", num1, num2, sum_result);
+	printf("\nSum of %.2f and %.2f is %.2f\n", num1, num2, sum_result);
 
 	sub_result = subtract(num1, num2);
 	printf("Difference of %.2f and %.2f is %.2f\n", num1, num2, sub_result);
+
+	div_result = divide(num1, num2);
+	printf("Division of %.2f and %.2f is %.2f\n", num1, num2, div_result);
 
 	mult_result = multiply(num1, num2);
 	printf("Product of %.2f and %.2f is %.2f\n", num1, num2, mult_result);
@@ -38,6 +42,11 @@ float add(float num1, float num2) {
 float subtract(float num1, float num2) {
 
 	return num1 - num2;
+}
+
+float divide(float num1, float num2) {
+
+	return num1 / num2;
 }
 
 float multiply(float num1, float num2) {
